ebfcbf4827bd0062f1b74e2c67197fb18ca1ed07
darylteo <i.am@darylteo.com>
2013-10-27 22:31:20 +1100

    Remove tests

--- edge-core/edge-core-java/src/test/java/org/edgeframework/core/tests/faces/StaticAssets.java
+++
@@ -1,9 +0,0 @@
-package org.edgeframework.core.tests.faces;
-
-import org.edgeframework.core.edges.types.StaticEdge;
-
-public class StaticAssets extends StaticEdge {
-  public StaticAssets() {
-    super("Static Assets", "localhost", 8080, "assets");
-  }
-}
