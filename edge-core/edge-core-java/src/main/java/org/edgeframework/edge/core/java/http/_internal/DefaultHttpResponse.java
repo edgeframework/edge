bc5594e3deb58700931021648bb3ed7df5ad1488
darylteo <i.am@darylteo.com>
2013-11-06 23:48:49 +1100

    Buffer response before output

--- edge-core/edge-core-java/src/main/java/org/edgeframework/edge/core/java/http/_internal/DefaultHttpResponse.java
+++ edge-core/edge-core-java/src/main/java/org/edgeframework/edge/core/java/http/_internal/DefaultHttpResponse.java
@@ -1,13 +1,17 @@
 package org.edgeframework.edge.core.java.http._internal;
 
 import org.edgeframework.edge.core.java.http.HttpResponse;
+import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.http.HttpServerResponse;
 
 public class DefaultHttpResponse implements HttpResponse {
   private HttpServerResponse vResponse;
 
+  private Buffer buffer;
+
   public DefaultHttpResponse(HttpServerResponse vResponse) {
     this.vResponse = vResponse;
+    this.buffer = new Buffer();
   }
 
   public DefaultHttpResponse statusCode(int code) {
@@ -25,14 +29,16 @@ public class DefaultHttpResponse implements HttpResponse {
   }
 
   public DefaultHttpResponse write(String content) {
-    this.vResponse.write(content);
+    this.buffer.appendString(content);
     return this;
   }
 
   public DefaultHttpResponse close() {
+    this.vResponse.headers().set("Content-Length", this.buffer.length() + "");
+    this.vResponse.write(this.buffer);
+
     this.vResponse.end();
     this.vResponse.close();
     return this;
   }
-
 }
